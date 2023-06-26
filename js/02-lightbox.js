060ea582f5184825b220241dddd7eef6ce565ff0
SophiaTretiakova <122561236+SophiaTretiakova@users.noreply.github.com>
2023-06-26 22:06:16 +0300

    second task complited

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const images = document.querySelector(".gallery");
+const html = [...galleryItems].reduce((accumulator, image) => {
+  return (
+    accumulator +
+    `<li class="gallery__item">
+   <a class="gallery__link" href="${image.original}">
+      <img class="gallery__image" src="${image.original}" alt="${image.description}" />
+   </a>
+</li>`
+  );
+}, "");
+images.insertAdjacentHTML("beforeend", html);
 
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
 console.log(galleryItems);
